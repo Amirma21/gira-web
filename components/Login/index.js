fc2f3541065023f5157599ca8d85cda9ef2c5be3
unknown <amirmaleki21physics@gmail.com>
2022-03-07 23:45:37 +0330

    Validated forms Login page

---
+++ components/Login/index.js
@@ -0,0 +1,90 @@
+import FormsLayout from "../common/FormsLayout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Link from "next/link";
+
+
+
+
+const initialValues = { email: "", password: "" };
+
+const validationSchema = Yup.object({
+  password: Yup.string(" ")
+    .required("رمز عبور را وارد کنید")
+    .min(8, " حداقل 8 حرف")
+    .max(20),
+  email: Yup.string()
+    .email("ایمیل معتبر وارد کنید")
+    .required("ایمیل را وارد کنید"),
+});
+
+const Login = () => {
+  const submitHandler = async (value) => {
+    console.log("loged");
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: submitHandler,
+    validationSchema,
+    validateOnMount: true,
+  });
+
+  return (
+    <>
+      <FormsLayout title={"ورود به سایت"}>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="flex flex-col w-full  px-4">
+            <div className="flex flex-col w-full my-2">
+              <input
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                type="email"
+                name="email"
+                className="py-3 px-2 border text-sm rounded-lg outline-purple-800 "
+                placeholder="ایمیل"
+              />
+              {formik.errors.email && formik.touched.email && (
+                <div className=" text-red-500 text-xs  my-1">
+                  {formik.errors.email}
+                </div>
+              )}
+            </div>
+
+            <div className="flex flex-col w-full my-2">
+              <input
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                type="password"
+                name="password"
+                className="py-3 px-2 border text-sm rounded-lg outline-purple-800 "
+                placeholder="رمز عبور"
+              />
+              {formik.errors.password && formik.touched.password && (
+                <div className=" text-red-500 text-xs  my-1">
+                  {formik.errors.password}
+                </div>
+              )}
+            </div>
+            <button
+              disabled={!formik.isValid}
+              className={`w-full p-1 rounded-lg my-4 text-white ${
+                formik.isValid
+                  ? "gradient  "
+                  : "bg-purple-500 cursor-not-allowed"
+              }`}
+            >
+              ورود
+            </button>
+
+            <div className="mt-4 mb-8"><Link href={"/register"}><h2 className="text-sm text-blue-500 cursor-pointer hover:font-bold transition-all ease-out">ایجاد حساب کاربری ؟</h2></Link></div>
+          </div>
+        </form>
+      </FormsLayout>
+    </>
+  );
+};
+
+export default Login;
