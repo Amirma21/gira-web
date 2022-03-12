a1d532561dc306ccf6a5a693a4c12ca9c16b3372
unknown <amirmaleki21physics@gmail.com>
2022-03-12 09:34:26 +0330

    do not show password in response

--- server/model/user.js
+++ server/model/user.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const {Schema} = mongoose
+
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    email : String,
-    password : String,
-    phoneNumber : Number ,
-    created : {type:Date , default:Date.now},
-})
+  name: String,
+  email: String,
+  password: String,
+  phoneNumber: Number,
+  created: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model("User" , userSchema)
+userSchema.set("toJSON" , {gettes:true})
+userSchema.options.toJSON.transform = (doc , ret)=>{
+    const obj = {...ret}
+    delete obj.password
+    delete obj._id 
+    delete obj._v
+    return obj
+}
+
+module.exports = mongoose.model.User || mongoose.model("User", userSchema);
